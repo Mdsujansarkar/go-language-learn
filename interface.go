5607968bfbc6c307bec3f9bfbf65faef67cf4dfe
Mdsujansarkar <sujansarkar786@gmail.com>
2023-02-22 23:32:31 +0600

    interface

--- interface.go
+++ interface.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type MotorVhical interface {
 	Milaze() float64
 }
@@ -17,4 +19,28 @@ type Audi struct {
 
 func (b Bmw) Milaze() float64 {
 	return b.distance / b.fuel
+}
+
+func (a Audi) Milaze() float64 {
+	return a.distance / a.fuel
+}
+func totalMotorMilaz(m []MotorVhical) {
+	tm := 0.0
+	for _, v := range m {
+		tm = tm + v.Milaze()
+	}
+	fmt.Printf("Your milaze is %f\n", tm)
+}
+func main() {
+	b1 := Bmw{
+		distance:     56.2,
+		fuel:         52.3,
+		averagespeed: "56.2",
+	}
+	a1 := Audi{
+		distance: 64.3,
+		fuel:     21.2,
+	}
+	persion := []MotorVhical{b1, a1}
+	totalMotorMilaz(persion)
 }
